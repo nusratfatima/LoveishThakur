d8848ea370e21faa19aa1ae0c56b25499669fe0d
Nusrat Malik <Nusrat.Malik@goapl.com>
2025-11-11 20:33:47 +0530

    feat: updated Header navigation links

--- app/sections/index.js
+++ app/sections/index.js
@@ -1,8 +1,15 @@
-// Re-export all sections as named exports (backed by defaults in each file)
-export { default as IntroSection } from "./introduction";
-export { default as BrandSection } from "./brands";
-export { default as ExperianceSection } from "./experiance";
-export { default as TechnologiesSection } from "./technologies";
-export { default as MoviesSection } from "./movies";
-export { default as VideoSection } from "./video";
-// export { default as ProjectSection } from "./project";
+import Introduction from "./sections/introduction";
+import Experience from "./sections/experiance";
+import Movies from "./sections/movies";
+import Video from "./sections/video";
+
+export default function HomePage() {
+  return (
+    <>
+      <section id="home"><Introduction /></section>
+      <section id="experience"><Experience /></section>
+      <section id="work"><Movies /></section>
+      <section id="contact"><Video /></section>
+    </>
+  );
+}
