19df7bf933710afed3ca3318e38cbcf348fa142a
Nusrat Malik <Nusrat.Malik@goapl.com>
2025-11-11 07:42:43 +0530

    updated the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,21 +9,22 @@ export function AppFooter() {
   return (
     <LazyMotion features={domAnimation}>
       <footer
-        className="container-md py-10 relative font-[avenir] before:absolute before:top-0 before:left-4 before:w-[calc(100%-16px)] before:h-[1px] before:bg-gray-100"
+        className="container-md py-10 relative font-[avenir] before:absolute before:top-0 before:left-4 before:w-[calc(100%-16px)] before:h-[1px] before:bg-gray-100 px-4 md:px-6"
         style={{
           backgroundColor: "#3971b8",
           color: "#fbfcee",
         }}
       >
-        <div className="flex flex-col md:flex-row justify-between items-center gap-10 md:gap-5">
-          <p className="font-light" style={{ color: "#343b1b" }}>
-            Copyright &copy; {year} Lavish Thakur
+        <div className="flex flex-col md:flex-row justify-start items-center gap-10 md:gap-5">
+          <p className="font-light" style={{ color: "#fbfcee" }}>
+            Copyright &copy; {year} Nusrat Fatima
           </p>
 
           <div className="flex flex-col md:flex-row items-center gap-5 text-sm">
             <a
               href="mailto:loveishvfx@gmail.com"
               className="flex items-center gap-2 hover:underline"
+              style={{ color: "#fbfcee" }}
             >
               <Mail size={18} /> loveishvfx@gmail.com
             </a>
@@ -32,6 +33,7 @@ export function AppFooter() {
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center gap-2 hover:underline"
+              style={{ color: "#fbfcee" }}
             >
               <Linkedin size={18} /> Linkedin
             </a>
@@ -40,12 +42,14 @@ export function AppFooter() {
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center gap-2 hover:underline"
+              style={{ color: "#fbfcee" }}
             >
               <Instagram size={18} /> Instagram
             </a>
             <a
               href="tel:+917669928801"
               className="flex items-center gap-2 hover:underline"
+              style={{ color: "#fbfcee" }}
             >
               <Phone size={18} /> +91 76699 28801
             </a>
