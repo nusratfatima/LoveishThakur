8e81f128b7d51f4fb8ac249a1d7d4e31a32e27c5
Nusrat Malik <Nusrat.Malik@goapl.com>
2025-11-12 20:27:04 +0530

    feat: full project with working sections and page setup

--- app/page.js
+++ app/page.js
@@ -1,23 +1,6 @@
 "use client";
-
-import {
-  IntroSection,
-  ExperianceSection,
-  TechnologiesSection,
-  BrandSection,
-  MoviesSection,
-  VideoSection,
-} from "./sections";
+import HomePage from "./sections";
 
 export default function Page() {
-  return (
-    <div className="container-md">
-      <IntroSection />
-      <ExperianceSection />
-      <TechnologiesSection />
-      <BrandSection />
-      <VideoSection />
-      <MoviesSection />
-    </div>
-  );
+  return <HomePage />;
 }
--- app/sections/index.js
+++ app/sections/index.js
@@ -1,7 +1,9 @@
-import Introduction from "./sections/introduction";
-import Experience from "./sections/experiance";
-import Movies from "./sections/movies";
-import Video from "./sections/video";
+import Introduction from "./introduction";
+import Experience from "./experiance";
+import Movies from "./movies";
+import Video from "./video";
+import Brands from "./brands";
+import Technologies from "./technologies";
 
 export default function HomePage() {
   return (
@@ -9,6 +11,8 @@ export default function HomePage() {
       <section id="home"><Introduction /></section>
       <section id="experience"><Experience /></section>
       <section id="work"><Movies /></section>
+      <section id="brands"><Brands /></section>
+      <section id="technologies"><Technologies /></section>
       <section id="contact"><Video /></section>
     </>
   );
